ce06f869466092ec786b770720c27dc2a1e9bcc9
Carlos <carlosmgomezj@gmail.com>
2026-02-14 18:25:44 +0100

    CMGJ-13 #time 2h Sintesis, implementacion y prueba rapida de practica EC22 en placa exitosa

--- projects/sandbox/src/rtl/TOP.vhd
+++ projects/sandbox/src/rtl/TOP.vhd
@@ -1,24 +1,19 @@
 --! \file TOP.vhd
 --! Archivo fuente VHDL.
---! \mainpage Proyecto HEX7SEG
+--! \mainpage Proyecto SECUENCIAL
 --! \section title1 Descripcion
 --! Pasos a realizar:
---! Nueva version del primer diseno:
---! - El convertidor de HEXADECIMAL a 7 segmentos
---! - Sin usar operadores l?gicos
---! - Valores distintos en los distintos digitos.
---!
---! Internamente el modelo incluira dos (2) modulos combinacionales:
---! - Un multiplexor 4 a 1 que seleccionara uno de los 4 vectores creados con ayuda de los alias. 
---! La salida del multiplexor (un vector de 4 bit, que es la senal interna declarada en la l?nea 40) sera la entrada al convertidor Hexadecimal a 7 segmentos.
---! El convertidor Hexadecimal a 7 segmentos, PERO ahora modelado con alguna de las sentencias estudiadas en este modulo, ya sea secuencial o concurrente.
+--! Primer diseno secuencial con:
+--! - Parametrizacion del diseno
+--! - Conexion con MUX del EC21
+--! - Diseno Jerarquico
 --! \note El formato de comentario incluye ! para poder realizar documentacion dinamica con Doxygen. 
 --! \section Uso
 --! Acceder a la documentacion abriendo el archivo <b>index.html</b> dentro de la carpeta ./docs/html. <br>
 --! En cada archivo (File), es posible acceder a su codigo fuente mostrado sin todos los comentarios pulsando en "Go to source code".
 --! \section source Codigo fuente
 --! La documentacion del codigo fuente se encuentra en \ref TOP.vhd
---! El codigo fuente se encuentra en <A HREF=_t_o_p_8vhd_source.html><B> TOP.vhd Annotated source </B></A>
+--! El codigo fuente se encuentra en <A HREF=_t_o_p_8vhd_source.html><B> TOP.vhd Annotated source </B></A> y en <A HREF=_c_d4_r_e_8vhd_source.html><B> CD4RE.vhd Annotated source </B></A>
 --! \section constraints Constraints
 --! El archivo de constraints se puede encontrar en \ref Basys3_GPIO.xdc
 --! \section reports Informes
@@ -26,7 +21,48 @@
 --! \section author Author
 --! Carlos Manuel Gomez Jimenez, DNI: 76037985P
 
-
+--! \page CalculoN Calculo del Generico N
+--!
+--! \section Objetivo
+--! Determinar el valor del generico \f$N\f$ para que el bit de mayor peso
+--! del contador free-running conmute con un periodo aproximado de
+--! \f$1\,\text{s}\f$, minimizando el error.
+--!
+--! \section Desarrollo
+--! El bit mas significativo (MSB) de un contador binario divide la frecuencia
+--! del reloj de entrada segun:
+--!
+--! \f[
+--! F_{MSB} = \frac{F_{clk}}{2^N}
+--! \f]
+--!
+--! Por tanto, el periodo asociado al MSB es:
+--!
+--! \f[
+--! T_{MSB} = \frac{1}{F_{MSB}} = \frac{2^N}{F_{clk}}
+--! \f]
+--!
+--! Si se desea un periodo aproximado de \f$T_{MSB} \approx 1\,\text{s}\f$,
+--! debe cumplirse:
+--!
+--! \f[
+--! 2^N \approx F_{clk}
+--! \f]
+--!
+--! Finalmente, el valor optimo de \f$N\f$ se obtiene como:
+--!
+--! \f[
+--! N \approx \log_2(F_{clk})
+--! \f]
+--! Para un reloj de \f$F_{clk} = 100\,\text{MHz}\f$:
+--!
+--! \f[
+--! N \approx \log_2(100 \times 10^6) \approx 26.58
+--! \f]
+--!
+--! Por tanto, el valor entero mas cercano es \f$N = 26\f$ o \f$N = 27\f$,
+--! siendo estos los que proporcionan el menor error usando divisiones
+--! por potencias de dos.
 --! \page Informe_E/S 
 --! \include io_report.txt
 --! \page Informe_Utilizacion
@@ -34,6 +70,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -46,8 +83,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 --! Entidad de que contiene las entradas y salidas
 entity TOP is
+    Generic (
+        --! \brief Tamano del vector contador calculado como se muestra en \ref CalculoN. 
+        --! Parametro que se define en la entidad de un modulo y permite personalizar ciertos aspectos del diseno sin cambiar el codigo interno. Es como una "variable de configuracion" para el modulo.
+        N: NATURAL := 26 
+        );
+          --! Clock Input
+    Port (CLK: in STD_LOGIC;
           --! Entrada de los 16 interruptores de tipo std_logic_vector, ordenado siendo el 15 el MSB.
-    Port (SW: in STD_LOGIC_VECTOR (15 downto 0);
+          SW: in STD_LOGIC_VECTOR (15 downto 0);
           --! \brief Entrada de los 5 botones de tipo std_logic_vector, ordenado siendo el 4 el MSB.
           --! \warning En la guia indica para el xdc: BTN[0]=BTNC; BTN[1]=BTNU; BTN[2]=BTNR; BTN[3]=BTND; BTN[4]=BTNL; No estan en orden.
           BTN: in STD_LOGIC_VECTOR (4 downto 0);
@@ -62,24 +106,50 @@ end TOP;
 
 --! Arquitectura de TOP.
 architecture Behavioral of TOP is
-    --! Cuatro interruptores (del SW0 al SW3) que definen el numero del CUARTO digito
-    alias SW_DIGIT_4: STD_LOGIC_VECTOR (3 downto 0) is SW(3 downto 0);
-    --! Cuatro interruptores (del SW4 al SW7) que definen el numero del TERCER digito
-    alias SW_DIGIT_3: STD_LOGIC_VECTOR (3 downto 0) is SW(7 downto 4);
-    --! Cuatro interruptores (del SW8 al SW11) que definen el numero del SEGUNDO digito
-    alias SW_DIGIT_2: STD_LOGIC_VECTOR (3 downto 0) is SW(11 downto 8);
-    --! Cuatro interruptores (del SW12 al SW15) que definen el numero del PRIMER digito
+    --! Vector declaration for counter
+    signal COUNTER: STD_LOGIC_VECTOR (N-1 downto 0);
+    --! primer grupo mas significativo de 4 bits del free-running que que definen el numero del TERCER digito (AN(1)) de izda a dcha.
+    alias SW_DIGIT_3: STD_LOGIC_VECTOR (3 downto 0) is COUNTER(N-1 downto N-4);
+    --! segundo grupo mas significativo de 4 bits del free-running que definen el numero del CUARTO digito (AN(0)) de izda a dcha.
+    alias SW_DIGIT_4: STD_LOGIC_VECTOR (3 downto 0) is COUNTER(N-5 downto N-8);
+    --! Cuatro interruptores (del SW8 al SW11) que definen el numero del SEGUNDO digito (AN(2)) de izda a dcha.
+    signal SW_DIGIT_2: STD_LOGIC_VECTOR (3 downto 0);
+    --! Cuatro interruptores (del SW12 al SW15) que definen el numero del PRIMER digito (AN(3)) de izda a dcha.
     alias SW_DIGIT_1: STD_LOGIC_VECTOR (3 downto 0) is SW(15 downto 12);
     --! Siete catodos (de A a G) que representan los siete segmentos de los displays
     signal CAT_NO_DP: STD_LOGIC_VECTOR (6 downto 0);
     --! Senal usada para conectar entre si dos modulos combinacionales internos
     signal COMB_CONN: STD_LOGIC_VECTOR (3 downto 0);
-    
-    
 
+    --! Senal de contador FREERUNING interna para hacer operaciones
+    signal Creg: UNSIGNED(N-1 downto 0);
+    
 begin
--- Asignamos cada LED a su interruptor correspondiente, indicando con la luz que el SW esta a 1.
-LED<=SW;
+
+
+------------------------------------------------------------------------------------------
+-- Instanciacion del modulo CD4RE
+-- Reloj heredado del MSB del contador free-running
+-- Reset y EN conectados como indica la especificacion.
+------------------------------------------------------------------------------------------
+U0: entity WORK.CD4RE
+port map (
+    C => COUNTER(N-1), 
+    R => SW(0),
+    CE => SW(1), 
+    Q => SW_DIGIT_2, 
+    TC => open,
+    CEO => open
+);
+
+--Contador Free running sin inicializacion
+
+Creg <= Creg +1 when rising_edge(CLK);
+COUNTER <= STD_LOGIC_VECTOR(Creg);
+
+-- Conectamos los 16 bit de mayor peso del contador a los LED
+LED<=COUNTER(N-1 downto N-16);
+
 -- Asignamos cada boton (exceptuando el boton central) al anodo de cada digito. 
 -- BTNL=DIGIT1, BTND=DIGIT2, BTNR=DIGIT3, BTNU=DIGIT4 (Digitos contados de izquierda a derecha)
 AN<= not BTN(4 downto 1);
