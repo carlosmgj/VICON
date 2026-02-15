9f99ad8a8fcac777b23223ba7930daa3745c1b36
Carlos <carlosmgomezj@gmail.com>
2026-02-15 17:57:12 +0100

    CMGJ-13 #time 2h Repaso de la practica de FIFO (por acabar)

--- projects/sandbox/src/rtl/TOP.vhd
+++ projects/sandbox/src/rtl/TOP.vhd
@@ -1,32 +1,54 @@
 --! \file TOP.vhd
 --! Archivo fuente VHDL.
---! \mainpage Proyecto SECUENCIAL
---! \image html diagram.svg "Diagrama logico"
+--! \mainpage Proyecto FIFO
 --! \section title1 Descripcion
---! Pasos a realizar:
+--! Modelado de una FIFO basada en memoria RAM
 --! Primer diseno secuencial con:
---! - Parametrizacion del diseno
---! - Conexion con MUX del EC21
---! - Diseno Jerarquico
+--! Definicion del interfaz de E/S de la FIFO
+--! - Modelado VHDL y sintesis de alguna de las alternativas de memoria interna de la FPGA.
+--! - Modelado VHDL de la logica de control
+--! - Modelado VHDL de la logica de estado
+--! \warning ESTE ES UN BLOQUE BASICO DE LOS QUE SE UTILIZAN EN EL TFM VICON
 --! \note El formato de comentario incluye ! para poder realizar documentacion dinamica con Doxygen. 
 --! \section Uso
 --! Acceder a la documentacion abriendo el archivo <b>index.html</b> dentro de la carpeta ./docs/html. <br>
 --! En cada archivo (File), es posible acceder a su codigo fuente mostrado sin todos los comentarios pulsando en "Go to source code".
 --! \section source Codigo fuente
 --! La documentacion del codigo fuente se encuentra en \ref TOP.vhd
---! El codigo fuente se encuentra en <A HREF=_t_o_p_8vhd_source.html><B> TOP.vhd Annotated source </B></A> y en <A HREF=_c_d4_r_e_8vhd_source.html><B> CD4RE.vhd Annotated source </B></A>
+--! El codigo fuente se encuentra en <A HREF=_t_o_p_8vhd_source.html><B> TOP.vhd Annotated source </B></A>.
 --! \section constraints Constraints
 --! El archivo de constraints se puede encontrar en \ref Basys3_GPIO.xdc
 --! \section reports Informes
 --! Tal y como pide la guia, los informes de interes se pueden encontrar en \ref Informe_E/S  y en \ref Informe_Utilizacion
 --! \section author Author
 --! Carlos Manuel Gomez Jimenez, DNI: 76037985P
-
 --! \page Informe_E/S 
 --! \include io_report.txt
 --! \page Informe_Utilizacion
 --! \include synthesis_utilization.txt
+--! \page lutram_vhdl 
+--! \section desc_push Detalle de la implementación
+--! - La única diferencia: NO existe la posibilidad de inicializar los registros
+--! - Se genera un bloque de RAM de doble puerto
+--! - No se genera lógica de decodificación y de multiplexación: Solo son necesarios 4 SLICE
+--! @verbatim
+--! process(CLK)
+--! begin
+--!     if rising_edge(CLK) then
+--!         if wr_en = '1' then 
+--!             array_reg(to_integer(unsigned(w_addr))) <= w_data;
+--!         end if;
+--!     end if;
+--! end process;
+--! r_data <= array_reg(to_integer(unsigned(r_addr)));
+--! @endverbatim
+--!
+--! - SI QUEREMOS CREAR MEMORIA LUTRAM Y NO USAR FF, EL MODELO NO DEBE CONTEMPLAR FUNCION DE RESET 
+--! - TAMPOCO SALDRÁ EN LA LISTA DE SENSIBILIDAD DEL PROCESO
+--! - LA LECTURA SE MODELA FUERA DEL PROCESO REGISTRADO
 
+    
+    
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -40,146 +62,112 @@ use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
---! Entidad de que contiene las entradas y salidas
-entity TOP is
+--! \brief Entidad de que contiene las entradas y salidas
+--! \details El interfaz del modulo FIFO debe incluir las E/S que se muestran en la figura adjunta
+--! \image html FIFO_ES.png "Diagrama logico"
+--! El interfaz del modulo FIFO debe incluir los genericos B y W que nos permiten
+--! parametrizar el tamano de la memoria RAM interna, tal y como se presento en el
+--! material didactico (modulo 32, pagina 5 de 8):
+--! - B es la anchura de bus de direcciones de la RAM interna (valor por defecto 6)
+--! - W es la anchura de los buses de datos (DIN y DOUT) (valor por defecto 8)
+
+entity FIFO is
     Generic (
-        --! \brief Tamano del vector contador calculado como se muestra en \ref CalculoN. 
-        --! Parametro que se define en la entidad de un modulo y permite personalizar ciertos aspectos del diseno sin cambiar el codigo interno. Es como una "variable de configuracion" para el modulo.
-        N: NATURAL := 26 
+        --! Anchura de bus de direcciones de la RAM interna
+        B: integer := 6;
+        --! Anchura de los buses de datos (DIN y DOUT)
+        W: integer := 8 
         );
-          --! Clock Input
+          --! Entrada de reloj
     Port (CLK: in STD_LOGIC;
-          --! Entrada de los 16 interruptores de tipo std_logic_vector, ordenado siendo el 15 el MSB.
-          SW: in STD_LOGIC_VECTOR (15 downto 0);
-          --! \brief Entrada de los 5 botones de tipo std_logic_vector, ordenado siendo el 4 el MSB.
-          --! \warning En la guia indica para el xdc: BTN[0]=BTNC; BTN[1]=BTNU; BTN[2]=BTNR; BTN[3]=BTND; BTN[4]=BTNL; No estan en orden.
-          BTN: in STD_LOGIC_VECTOR (4 downto 0);
-          --! Salida de los 16 LEDs de tipo std_logic_vector, ordenado siendo el 15 el MSB.
-          LED: out STD_LOGIC_VECTOR (15 downto 0);
-          --! Salida de los 8 catodos de los displays de tipo std_logic_vector, ordenado siendo el 7 el MSB.
-          CAT: out STD_LOGIC_VECTOR (7 downto 0);
-          --! Salida de los 4 anodos de los displays de tipo std_logic_vector, ordenado siendo el 3 el MSB.
-          AN: out STD_LOGIC_VECTOR (3 downto 0));
-end TOP;
+          --! Entrada de reset.
+          RST: in STD_LOGIC;
+          --! Entrada de DATOS
+          DIN: in STD_LOGIC_VECTOR (W-1 downto 0);
+          --! Senal para meter un dato en la FIFO (Presente en DIN)
+          PUSH: in STD_LOGIC;
+          --! Senal que indica que la FIFO esta llena (1)
+          FULL: out STD_LOGIC;
+          --! Salida de DATOS
+          DOUT: out STD_LOGIC_VECTOR (W-1 downto 0);
+          --! Senal para sacar un dato de la FIFO (Estará en DOUT);
+          POP: in STD_LOGIC;
+          --! Senal que indica que la FIFO esta vacia (1)
+          EMPTY: out STD_LOGIC
+          ); 
+end FIFO;
 
 
---! Arquitectura de TOP.
-architecture Behavioral of TOP is
-    
-     --! Vector declaration for counter
-    signal COUNTER: NATURAL range 0 to N-1 := 0;
-    --! primer grupo mas significativo de 4 bits del free-running que que definen el numero del TERCER digito (AN(1)) de izda a dcha.
-    --! Cuatro interruptores (del SW8 al SW11) que definen el numero del SEGUNDO digito (AN(2)) de izda a dcha.
-    signal SW_DIGIT_2: STD_LOGIC_VECTOR (3 downto 0);
-    --! Cuatro interruptores (del SW12 al SW15) que definen el numero del PRIMER digito (AN(3)) de izda a dcha.
-    alias SW_DIGIT_1: STD_LOGIC_VECTOR (3 downto 0) is SW(15 downto 12);
-    --! Siete catodos (de A a G) que representan los siete segmentos de los displays
-    signal CAT_NO_DP: STD_LOGIC_VECTOR (6 downto 0);
-    --! Senal usada para conectar entre si dos modulos combinacionales internos
-    signal COMB_CONN: STD_LOGIC_VECTOR (3 downto 0);
-    
-    -- Senales necesarias para la nueva instancia 
-    signal MCLK,locked,MRST: STD_LOGIC;
-    alias reset_in: STD_LOGIC is BTN(0);
-    -- Nueva senal de habilitacion
-    signal BCD_EN: STD_LOGIC;
-
+--! Arquitectura de la FIFO.
+architecture Behavioral of FIFO is
+    --! ARRAY de VECTORES de LONGITUD 2^B
+    type fifo_mem is array(0 to (2**B)-1) of STD_LOGIC_VECTOR(W-1 downto 0);
+    --! Senal que modela la RAM (Lineas de teoria: \ref lutram_vhdl)
+    signal mem: fifo_mem := (others=> (others=>'0'));
+    --! Contador/puntero de lectura
+    signal rd_ptr: INTEGER range 0 to (2**B)-1 := 0;
+    --! Contador/puntero de escritura
+    signal wr_ptr: INTEGER range 0 to (2**B)-1 := 0;
+    --! Senal interna de full \ref FIFO::FULL
+    signal fifo_full: STD_LOGIC := '0';
+    --! Senal interna de empty \ref FIFO::EMPTY
+    signal fifo_empty: STD_LOGIC := '0';
+    --! Senal de habilitacion de escritura en la RAM
+    signal wr_en: STD_LOGIC := '0';
+    --! Senal de habilitacion de lectura en la RAM
+    signal rd_en: STD_LOGIC := '0';
+   
 begin
 
-------------------------------------------------------------------------------------------
--- Instanciacion de MMCM
-------------------------------------------------------------------------------------------
-miMMCM: entity WORK.clk_wiz_0
-port map (
-    clk_in1 => CLK,
-    reset => reset_in,
-    clk_out1 => MCLK,
-    locked => locked
-);
+    -- MODELADO DEL BLOQUE DE RAM INTERNO
+    -- LUTRAM = Memoria Distribuida
 
-------------------------------------------------------------------------------------------
--- Instanciacion del modulo CD4RE
--- Reloj heredado del MSB del contador free-running
--- Reset y EN conectados como indica la especificacion.
-------------------------------------------------------------------------------------------
-U0: entity WORK.CD4RE
-port map (
-    C => MCLK, 
-    R => MRST,
-    CE => BCD_EN, 
-    Q => SW_DIGIT_2, 
-    TC => open,
-    CEO => open
-);
-
-------------------------------------------------------------------------------------------
--- Contador Free Running SIN INICIALIZACION
-------------------------------------------------------------------------------------------
-process(MCLK)
+    -- Proceso de escritura (PUSH)
+    process(CLK, RST)
     begin
-        if rising_edge(MCLK) then
-            -- No se habla del reset del contador free-running, pero si no se hace no se reinicia la cuenta al resetear.
-            -- if reset_in = '1' or counter = N-1 then 
-            if counter = N-1 then 
-                -- Reiniciamos el contador al alcanzar el maximo
-                counter <= 0;
-            else
-                -- Incrementamos el contador
-                counter <= counter + 1;
+        if RST = '1' then
+            wr_ptr <= 0;
+            fifo_full <= '0';
+        elsif rising_edge(CLK) then
+            if (PUSH = '1' and  fifo_full='0') then
+                mem(wr_ptr) <= DIN;
+                wr_ptr <= (wr_ptr + 1) mod (2**B);  -- Incrementar y hacer el wrap-around
             end if;
         end if;
-end process;
+    end process;
 
---Enable dura el ciclo de reloj que dura el contador en el numero maximo
-BCD_EN <= '1' when counter = N-1 else '0';
+    -- Proceso de lectura (POP)
+    process(CLK, RST)
+    begin
+        if RST = '1' then
+            rd_ptr <= 0;
+            fifo_empty <= '1';
+        elsif rising_edge(CLK) then
+            if POP = '1' and fifo_empty='0' then
+                DOUT <= mem(rd_ptr);
+                rd_ptr <= (rd_ptr + 1) mod (2**B);  -- Incrementar y hacer el wrap-around
+            end if;
+        end if;
+    end process;
 
--- La se?al LOCKED la visualizaremos en el punto del display -CAT(7)-
-CAT(7) <= not locked;
--- Asignacion de MRST como indica en la guia, cuando no est? locked el MMCM.
-MRST <= not locked;
+    -- L?gica para los indicadores FULL y EMPTY
+    process(wr_ptr, rd_ptr)
+    begin
+        if wr_ptr = rd_ptr then
+            fifo_empty <= '1';  -- FIFO vac?a si los punteros son iguales
+            fifo_full <= '0';
+        elsif (wr_ptr + 1) mod (2**B) = rd_ptr then
+            fifo_empty <= '0';
+            fifo_full <= '1';  -- FIFO llena si el puntero de escritura ha alcanzado el puntero de lectura
+        else
+            fifo_empty <= '0';
+            fifo_full <= '0';  -- FIFO no está llena ni vac?a
+        end if;
+    end process;
 
---! Asignamos boton izdo y boton de abajo al anodo de los digitos 1 y 2. 
---! BTNL=DIGIT1, BTND=DIGIT2, BTNR=DIGIT3, BTNU=DIGIT4 (Digitos contados de izquierda a derecha)
-AN(3 downto 2)<= not BTN(4 downto 3);
---! AN0 y AN1 los mantendremos apagados
-AN(1 downto 0)<= (others=>'1');
-
---! Multiplexor
---! En los requisitos no se contempla que se puedan pulsar m?s de un boton a la vez, la aproximacion asumida es la siguiente 
---! pone el contador en los dos si se pulsan los dos botones a la vez
---! OPC1:
-with BTN(4 downto 1) select
-COMB_CONN <= SW_DIGIT_1 when "1000",
-             SW_DIGIT_2 when others;
-
---! Conversor de HEX a 7 segmentos utilizando la tabla de verdad.
---! El orden esta invertido ya que CAT(0) corresponde al a que es ahora el bit menos significativo.             
-with COMB_CONN(3 downto 0) select
-CAT_NO_DP <= "1000000" when "0000",
-             "1111001" when "0001",
-             "0100100" when "0010",
-             "0110000" when "0011",
-             -----
-             "0011001" when "0100",
-             "0010010" when "0101",
-             "0000010" when "0110",
-             "1111000" when "0111",
-             -----
-             "0000000" when "1000",
-             "0010000" when "1001",
-             "0001000" when "1010",
-             "0000011" when "1011",
-             ------
-             "1000110" when "1100",
-             "0100001" when "1101",
-             "0000110" when "1110",
-             "0001110" when others;
-
---! Asignamos a los catodos el valor de la senal de salida del conversor.
-CAT(6 downto 0)<= CAT_NO_DP;     
-LED<=(others=>'0');
-
-    
+    -- Asignaci?n de se?ales de salida
+    FULL <= fifo_full;
+    EMPTY <= fifo_empty;
 end Behavioral;
 
 
